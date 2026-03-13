b6e78decee2e1d05f2fefd1b40d2ba02b9134501
Faridsz0605 <farid.s.villa@gmail.com>
2026-03-13 17:21:13 -0500

    implementacion de agente y To Dos

--- agent.py
+++ agent.py
@@ -14,6 +14,10 @@ LR = 0.001
 
 class Agent:
     def __init__(self) -> None:
+        self.n_games = 0
+        self.epsilon = 0 # parameter to control randomness
+        self.memory = deque(max len=MAX_MEMORY)
+        # TO DO 
         pass
 
     def get_state(self, game):
@@ -30,3 +34,16 @@ class Agent:
 
     def get_action(self):
         pass
+
+def train():
+  plot_score = []
+  plot_mean_score = []
+  tot_score = []
+  record = 0
+  agent = Agent()
+  game = SnakeGameAI()
+  while True:
+    state_old = agent.get_state(game)
+    
+                                final_move = agent.get_action(state_old)
+  pass
